0dabb1f224954952bee1b4e5f9961338f72be3c3
Rolando <jrdosmil10@gmail.com>
2020-04-28 11:26:09 -0500

    First commit

---
+++ server/config/config.js
@@ -0,0 +1,4 @@
+//Set up port
+
+process.env.PORT = process.env.PORT || 3000
+//This sets up the port. Automatly can be 3000 or the url procided by heroku
---
+++ server/server.js
@@ -0,0 +1,57 @@
+//This makes the same thing than https library
+const express = require('express')
+const bodyParser = require('body-parser')
+//With bodyparser I can read the body from a request
+
+require('./config/config')
+
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(bodyParser.json())
+
+//This '/' is the adress to make a request
+app.get('/user', function (req, res) {
+    res.json('Getting an user specified')
+})
+
+
+app.put('/user/:id', (req, res) => {
+
+    let id = req.params.id
+    res.json({
+        id //Not necessary add a value this is automatically added
+    })
+
+
+})
+
+app.post('/user/', (req, res) => {
+
+    let body = req.body
+
+    let name = body.name
+    let erroCode = 400
+
+    //In postman x-wwwform=rlendocded
+
+    if (name === undefined) {
+
+        res.status(erroCode).json(
+            {
+                err: 400,
+                msg: 'We need a name to make insertion'
+            }
+        )
+    } else {
+        res.json({
+            body
+        })
+    }
+})
+
+
+app.listen(process.env.PORT,() => {
+    console.log('Listen at port :', process.env.PORT);
+})
